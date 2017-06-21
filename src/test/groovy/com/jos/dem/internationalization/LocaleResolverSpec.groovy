67bf1d3a7d8aa98a345f2800b2e76916ea2f795b
josdem <joseluis.delacruz@gmail.com>
2017-06-21 10:47:00 -0500

    Adding spock test

---
+++ src/test/groovy/com/jos/dem/internationalization/LocaleResolverSpec.groovy
@@ -0,0 +1,42 @@
+package com.jos.dem.internationalization
+
+import javax.servlet.http.HttpServletRequest
+
+import com.jos.dem.vetlog.helper.LocaleResolver
+
+import spock.lang.Specification
+
+class LocaleResolverSpec extends Specification {
+
+	LocaleResolver resolver = new LocaleResolver()
+
+	void "should get locale default"(){
+		given:'A request'
+		  HttpServletRequest request = Mock(HttpServletRequest)
+    when:'We call resolve'
+      Locale result = resolver.resolveLocale(request)
+    then:'We expect default'
+      Locale.getDefault() == result
+	}
+
+  void "should get en-US as locale"(){
+		given:'A request'
+		  HttpServletRequest request = Mock(HttpServletRequest)
+    when:'We call resolve'
+      request.getHeader('Accept-Language') >> 'en-US,en;q=0.8'
+      Locale result = resolver.resolveLocale(request)
+    then:'We expect default'
+      result  == new Locale('en')
+	}
+
+  void "should get es-MX as locale"(){
+		given:'A request'
+		  HttpServletRequest request = Mock(HttpServletRequest)
+    when:'We call resolve'
+      request.getHeader('Accept-Language') >> 'es-MX,en-US;q=0.7,en;q=0.3'
+      Locale result = resolver.resolveLocale(request)
+    then:'We expect default'
+      result  == new Locale('es')
+	}
+
+}
