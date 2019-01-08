7b8b1d3ca0f6fc5857a47b5819967a2824127f7b
josdem <joseluis.delacruz@gmail.com>
2019-01-08 17:21:31 -0500

    Updating project information

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 Spring Boot Internationalization
 ----------------------------------------
 
+How to implement Internationalization in a Spring Boot Project
+
+#### To run the project
+
+```bash
+gradle bootRun
+```
+
+#### To test the project
+
+```bash
+gradle test
+```
 #### Read this as reference:
 
 * http://josdem.io/techtalk/spring/spring_boot_internationalization/
-* http://josdem.io/techtalk/spring/spring_boot/
--- src/test/java/com/jos/dem/springboot/internationalization/LocaleResolverTest.java
+++ src/test/java/com/jos/dem/springboot/internationalization/LocaleResolverTest.java
@@ -19,7 +19,7 @@ import com.jos.dem.springboot.internationalization.helper.LocaleResolver;
 class LocaleResolverTest {
 
   @InjectMocks
-	private LocaleResolver resolver = new LocaleResolver();
+  private LocaleResolver resolver = new LocaleResolver();
 
   @Mock
   private HttpServletRequest request;
@@ -30,11 +30,11 @@ class LocaleResolverTest {
   }
 
   @Test
-	@DisplayName("should get locale default")
+  @DisplayName("should get locale default")
   void shouldGetDefaultLocale() {
     Locale result = resolver.resolveLocale(request);
     assertEquals(Locale.getDefault(), result);
-	}
+  }
 
   @Test
   @DisplayName("should get en-US as locale")
@@ -42,7 +42,7 @@ class LocaleResolverTest {
     when(request.getHeader("Accept-Language")).thenReturn("en-US,en;q=0.8");
     Locale result = resolver.resolveLocale(request);
     assertEquals(new Locale("en"), result);
-	}
+  }
 
   @Test
   @DisplayName("should get es-MX as locale")
@@ -50,6 +50,6 @@ class LocaleResolverTest {
     when(request.getHeader("Accept-Language")).thenReturn("es-MX,en-US;q=0.7,en;q=0.3");
     Locale result = resolver.resolveLocale(request);
     assertEquals(new Locale("es"), result);
-	}
+  }
 
 }
